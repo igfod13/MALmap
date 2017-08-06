c52d03644689d450092560065251d8fb533b2a89
igfod13 <igfod13@gmail.com>
2017-08-05 22:45:54 -0700

    Added manga map

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MALmap
 An interactive map of anime based on MyAnimeList recommendations.
 
-If you wish to see your MyAnimeList mapped to the graph, download the repo and run the python script or the last cell of the Jupyter notebook (or ask me for the data file), then open user_map.html(only works in Firefox or on a webserver).
+If you wish to see your MyAnimeList mapped to the graph, download the repo and run the python script user_export.py for anime or user_export_manga.py for manga(or ask me for the data file), then open index.html for anime and manga.html for manga(only works in Firefox or on a webserver).
---
+++ user_export_manga.py
@@ -0,0 +1,136 @@
+import re
+import time
+import numpy as np
+import requests
+import xml.etree.ElementTree as ET
+import json
+import collections
+import pickle
+import os
+import sys
+
+class Manga:
+    def __init__(self, id_num):
+        self.id_num = id_num
+
+# Export user's MAL network graph
+
+# Import anime_list object
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(sys.argv[0])))
+manga_list = pickle.load(open(os.path.join(__location__, 'save_manga.p'), 'rb'))
+
+# username = "igfod13"
+try:
+	username = raw_input("Username: ")
+except:
+	username = input("Username: ")
+	
+# Load user's anime list
+url = "https://myanimelist.net/malappinfo.php?status=all&type=manga&u=" + username
+response = requests.get(url)
+# print (response.status_code)
+tree = ET.fromstring(response.content)
+
+# Retrieve user data
+user_list = {}
+user_score_list ={}
+for manga in tree:
+    for item in manga:
+        if item.tag == 'series_mangadb_id':
+            temp_id = int(item.text)
+        if item.tag == 'my_status':
+            temp_status = int(item.text)
+        if item.tag == 'my_score':
+            temp_score = int(item.text)
+    if manga.tag == 'manga':
+        user_list[temp_id] = temp_status
+        user_score_list[temp_id] = temp_score
+
+# Get user's score distribution
+user_data = []    
+for i, a in user_score_list.items():
+    if a != 0:
+        user_data.append(a)
+a = np.array(user_data)
+user_percentile = np.percentile(a, 65)
+
+# Get total score distribution
+data = []
+for cur_id, a in manga_list.items():
+    data.append(a.score)
+a = np.array(data)
+p1 = np.percentile(a, 90)
+p2 = np.percentile(a, 70)
+p3 = np.percentile(a, 40) 
+
+# MAL status IDS: 1 = watching, 2 = completed, 3 = on hold, 4 = dropped, 6 = plan to watch
+# Additional IDS: 9 = highly ranked by user, 10-13 = top 0-10/10-30/30-60/60-100% ranked unwatched
+for cur_id, a in manga_list.items():
+    if cur_id in user_list:
+        if user_list[cur_id] == 2 and user_score_list[cur_id] > user_percentile:
+                a.user_status = 9
+        else:
+            a.user_status = user_list[cur_id]
+    else:
+        if a.score > p1:
+            a.user_status = 10
+        elif a.score > p2:
+            a.user_status = 11
+        elif a.score > p3:
+            a.user_status = 12
+        else:
+            a.user_status = 13
+            
+# Load exported data file from Gephi
+with open(os.path.join(__location__, 'manga_data.json')) as data_file:
+    sigma_data = json.load(data_file)
+    data_file.close()
+
+# Modify attributes
+for d in sigma_data['nodes']:
+    cur_id = int(d['id'])
+    a = manga_list.get(cur_id)
+    attr = collections.OrderedDict()
+    attr['Score'] = a.score
+    attr['Rank'] = '#' + str(a.rank)
+    attr['Popularity'] = '#' + str(a.popularity)
+    attr['Members'] = '{:,}'.format(a.members)
+    genre_str = '';
+    for g in a.genre_names:
+        if genre_str is not '':
+            genre_str = genre_str + ', '
+        genre_str = genre_str + g
+    attr['Genres'] = genre_str
+    cur_url = '<a href="https://myanimelist.net/manga/' + str(cur_id) + '">' + a.name + '</a>'
+    attr['MAL Link'] = cur_url
+    attr['Synopsis'] = a.synopsis
+    attr['Image'] = a.img_url
+    d['attributes'] = attr
+    # Set node colors
+    if a.user_status == 1:
+        d['color'] = 'rgb(45,175,55)'
+    elif a.user_status == 2:
+        d['color'] = 'rgb(40,80,210)'
+    elif a.user_status == 3:
+        d['color'] = 'rgb(250,210,85)'
+    elif a.user_status == 4:
+        d['color'] = 'rgb(160,45,50)'
+    elif a.user_status == 6:
+        d['color'] = 'rgb(255,155,45)'
+    elif a.user_status == 9:
+        d['color'] = 'rgb(50,190,255)'
+    elif a.user_status == 10:
+        d['color'] = 'rgb(195,195,195)'
+    elif a.user_status == 11:
+        d['color'] = 'rgb(155,155,155)'
+    elif a.user_status == 12:
+        d['color'] = 'rgb(115,115,115)'
+    else:
+        d['color'] = 'rgb(75,75,75)'
+    
+for d in sigma_data['edges']:
+    d['color'] = 'rgb(75,75,75)'
+
+# Export new data file
+with open(os.path.join(__location__, 'manga_data.json'), 'w') as outfile:  
+    json.dump(sigma_data, outfile)
